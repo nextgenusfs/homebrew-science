dc5648cdb828ac2f97d5b7430384f99eff44c42e
Jon Palmer <nextgen.usfs@gmail.com>
2016-09-27 14:19:16 -0500

    Add mirror download link for repeatscout
    official link is currently offline, adding mirror hosted at RepeatMasker

--- repeatscout.rb
+++ repeatscout.rb
@@ -4,6 +4,7 @@ class Repeatscout < Formula
   # tag "bioinformatics"
 
   url "http://repeatscout.bioprojects.org/RepeatScout-1.0.5.tar.gz"
+  mirror "http://www.repeatmasker.org/RepeatScout-1.0.5.tar.gz"
   sha256 "bda6f782382f2b7dcb6a004b7da586d5046b3c12429b158e24787be62de6199c"
 
   bottle do
@@ -27,3 +28,4 @@ class Repeatscout < Formula
     system "#{bin}/RepeatScout 2>&1 |grep RepeatScout"
   end
 end
+
